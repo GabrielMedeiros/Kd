818ebaffa9d7d210b7eea7f05f6e91f70ba097df
Gabriel M. Cardozo <gabriel.medeiros@vtex.com.br>
2013-07-02 18:42:26 -0300

    Finder iniciado

---
+++ src/Vtex.Kd.Test/StaFinderTest.cs
@@ -0,0 +1,95 @@
+﻿using NUnit.Framework;
+
+namespace Vtex.Kd.Test
+{
+    [TestFixture]
+    public class StaFinderTest
+    {
+        [Test]
+        public void Sta_at_Mezzanine_return_Mezzanine()
+        {
+            var sta = new UnifiSta()
+            {
+                oui = "test",
+                noise = -95,
+                signal = -25,
+                ap_mac = "1:1:1"
+            };
+
+            var staFinder = new StaFinder();
+
+            var placeInfo = staFinder.Find(sta);
+
+            Assert.AreEqual(PlaceEnum.Mezzanine, placeInfo.Place);
+        }
+
+        [Test]
+        public void Sta_at_2_floor_return_2_floor()
+        {
+
+            var sta = new UnifiSta()
+            {
+                oui = "test",
+                noise = -95,
+                signal = -65,
+                ap_mac = "1:1:1"
+            };
+
+            var staFinder = new StaFinder();
+
+            var placeInfo = staFinder.Find(sta);
+
+            Assert.AreEqual(PlaceEnum.SecondFloor, placeInfo.Place);
+
+        }
+        
+        [Test]
+        public void Sta_at_10_floor_return_10_floor()
+        {
+            var sta = new UnifiSta()
+            {
+                oui = "test",
+                noise = -95,
+                signal = -25,
+                ap_mac = "2:2:2"
+            };
+
+            var staFinder = new StaFinder();
+
+            var placeInfo = staFinder.Find(sta);
+
+            Assert.AreEqual(PlaceEnum.TenthFloor, placeInfo.Place);
+        }
+
+    }
+
+    public class StaFinder
+    {
+        public PlaceInfo Find(UnifiSta sta)
+        {
+            var placeInfo = new PlaceInfo();
+
+            if (sta.signal == -25)
+                if (sta.ap_mac == "2:2:2")
+                    placeInfo.Place = PlaceEnum.TenthFloor;
+                else
+                    placeInfo.Place = PlaceEnum.Mezzanine;
+            else
+                placeInfo.Place = PlaceEnum.SecondFloor;
+
+            return placeInfo;
+        }
+    }
+
+    public class PlaceInfo
+    {
+        public PlaceEnum Place { get; set; }
+    }
+
+    public enum PlaceEnum
+    {
+        Mezzanine,
+        SecondFloor,
+        TenthFloor
+    }
+}
--- src/Vtex.Kd/UnifiSta.cs
+++ src/Vtex.Kd/UnifiSta.cs
@@ -1,9 +1,39 @@
 ﻿
+using System;
+using System.Net.Configuration;
+
 namespace Vtex.Kd.Test
 {
     public class UnifiSta
     {
         public string oui { get; set; }
+        public string ap_mac { get; set; }
+        public int noise { get; set; }
+        public int signal { get; set; }
 
+        protected bool Equals(UnifiSta other)
+        {
+            return string.Equals(oui, other.oui) && string.Equals(ap_mac, other.ap_mac) && noise == other.noise && signal == other.signal;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((UnifiSta) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = (oui != null ? oui.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (ap_mac != null ? ap_mac.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ noise;
+                hashCode = (hashCode*397) ^ signal;
+                return hashCode;
+            }
+        }
     }
 }
